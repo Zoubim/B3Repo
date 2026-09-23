d7e5445a22a5ae66a7a0eb6913b1c56725d19971
[Mohannad Alzubi] <[zoubim@live.com]>
2026-09-23 11:11:03 +1000

    B3.2 created four strings

---
+++ partB3.js
@@ -0,0 +1,26 @@
+/**
+ * Part B3 - String Manipulation
+ * JavaScript Assessment 2.
+ */
+
+
+/**
+ * ------------
+ * B3.2 - Define the strings
+ * ------------
+ */
+
+// myString created
+const myString = "This is a string";
+// anotherString created with three spaces at the 
+// start
+const anotherString = "   Another string"
+// Hello there string as per the question
+const hello = "Hello there!";
+// myName stores my actual real name Mohannad
+const myName = "Mohannad";
+
+console.log("B3.2 myString      :", myString);
+console.log("B3.2 anotherString :", anotherString);
+console.log("B3.2 hello         :", hello);
+console.log("B3.2 myName        :", myName);
