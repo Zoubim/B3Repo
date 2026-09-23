0ed92393a146be6c2d2e695fdbe84e06edd25597
[Mohannad Alzubi] <[zoubim@live.com]>
2026-09-23 11:22:12 +1000

    B3.3 length, first and 11th chars from myString

--- partB3.js
+++ partB3.js
@@ -24,3 +24,23 @@ console.log("B3.2 myString      :", myString);
 console.log("B3.2 anotherString :", anotherString);
 console.log("B3.2 hello         :", hello);
 console.log("B3.2 myName        :", myName);
+
+/**
+ * ------------
+ * B3.3 - Utilities to find further info 
+ * ------------
+ */
+
+// Store length of myString in stringLength so
+// we can keep consistent console logs rather than
+// using myString.length directly
+//console.log(myString.length);
+const stringLength = myString.length;
+// First character of myString
+const firstCharacter = myString.charAt(0);
+// 11th character of myString
+const eleCharacter = myString.charAt(10);
+
+console.log("B3.3 Length of myString: ", stringLength);
+console.log("B3.3 First character:    ", firstCharacter);
+console.log("B3.3 Eleventh character: ", eleCharacter);
